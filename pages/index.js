797ad1dc74b1d309606dfc5dfd7426415d57b17b
morf77 <mhamadreza77@gmail.com>
2022-10-24 15:59:51 +0330

    adding some components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import MeetupList from '../components/meetups/MeetupList'
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+    const dummy_meetups = [
+        {
+            id: 'm1',
+            title: 'first meet up',
+            image : 'https://upload.wikimedia.org/wikipedia/commons/8/8b/PanoStanislasCrepuscule.jpg',
+            address : 'some address 5, 12345 sime city'
+        },
+        {
+            id: 'm1',
+            title: 'first meet up',
+            image : 'https://upload.wikimedia.org/wikipedia/commons/8/8b/PanoStanislasCrepuscule.jpg',
+            address : 'some address 5, 12345 sime city'
+        }
+    ]
+
+  return <MeetupList meetups={dummy_meetups} />;
 }
 
 export default HomePage;
